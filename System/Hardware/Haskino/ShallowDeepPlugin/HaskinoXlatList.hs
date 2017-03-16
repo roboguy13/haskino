3d35e75ef74e610c6106eeedf832bd58820c8a0d
David Young <roboguy06@gmail.com>
2017-03-16 11:52:54 -0500

    Workaround for TH-related linking errors

--- System/Hardware/Haskino/ShallowDeepPlugin/HaskinoXlatList.hs
+++ System/Hardware/Haskino/ShallowDeepPlugin/HaskinoXlatList.hs
@@ -6,7 +6,7 @@
 -- Stability   :  experimental
 --
 -------------------------------------------------------------------------------
-{-# LANGUAGE TemplateHaskell #-}
+
 module System.Hardware.Haskino.ShallowDeepPlugin.HaskinoXlatList where
 
 import CoreMonad
@@ -15,33 +15,33 @@ import Language.Haskell.TH
 
 import System.Hardware.Haskino.ShallowDeepPlugin.Utils
 
+import System.Hardware.Haskino.ShallowDeepPlugin.XlatEntry
+
 import Data.Boolean
 import System.Hardware.Haskino
-
-import System.Hardware.Haskino.ShallowDeepPlugin.XlatEntry
 
 -- The following table defines the names of the Shallow DSL functions
 -- to translate from and the Deep DSL functions to translate to.
 xlatList :: [XlatEntry]
-xlatList = [  XlatEntry (thNameToId 'not)
-                        (thNameToId 'Data.Boolean.notB)
-            , XlatEntry (thNameToId '(||))
-                        (thNameToId '(||*))
-            , XlatEntry (thNameToId '(&&))
-                        (thNameToId '(&&*))
-            , XlatEntry (thNameToId '(==))
-                        (thNameToId 'eqE)
-            , XlatEntry (thNameToId '(/=))
-                        (thNameToId 'neqE)
-            , XlatEntry (thNameToId '(>))
-                        (thNameToId 'greatE)
-            , XlatEntry (thNameToId '(<))
-                        (thNameToId 'lessE)
-            , XlatEntry (thNameToId '(>=))
-                        (thNameToId 'greateqE)
-            , XlatEntry (thNameToId '(<))
-                        (thNameToId 'lesseqE)
-            , XlatEntry (thNameToId '(+))
-                        (thNameToId '(+))
+xlatList = [  XlatEntry (stringToId "not")
+                        (stringToId "Data.Boolean.notB")
+            , XlatEntry (stringToId "(||)")
+                        (stringToId "(||*)")
+            , XlatEntry (stringToId "(&&)")
+                        (stringToId "(&&*)")
+            , XlatEntry (stringToId "(==)")
+                        (stringToId "eqE")
+            , XlatEntry (stringToId "(/=)")
+                        (stringToId "neqE")
+            , XlatEntry (stringToId "(>)")
+                        (stringToId "greatE")
+            , XlatEntry (stringToId "(<)")
+                        (stringToId "lessE")
+            , XlatEntry (stringToId "(>=)")
+                        (stringToId "greateqE")
+            , XlatEntry (stringToId "(<)")
+                        (stringToId "lesseqE")
+            , XlatEntry (stringToId "(+)")
+                        (stringToId "(+)")
            ]
 
