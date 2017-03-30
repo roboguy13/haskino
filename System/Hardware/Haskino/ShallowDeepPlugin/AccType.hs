1e12a4ffc37b220479fae7d4c1d65baa7366c8a0
David Young <roboguy06@gmail.com>
2017-03-30 00:49:13 -0500

    Another idea for kicking off the transformation for Accelerate (and in a more generalized fashion)

--- System/Hardware/Haskino/ShallowDeepPlugin/AccType.hs
+++ System/Hardware/Haskino/ShallowDeepPlugin/AccType.hs
@@ -9,3 +9,14 @@ data AccS a b
   = AccDone (Acc (Plain b))
   | AccStep (Acc (Plain a)) (AccS (Acc (Plain a)) (a -> b))
 
+
+
+
+
+-- Another idea (use a run function and combine the "transform everything
+-- of a certain type approach" and the "use a run function and push rep in"
+-- approach)
+
+run :: a -> Acc a
+run _ = undefined -- TODO: Implement
+
